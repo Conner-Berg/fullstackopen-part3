edd48baa32d7f4245527d94571399dc50374645c
Conner-Berg <connerwberg@gmail.com>
2023-04-07 10:57:44 -0400

    Finish 3.4, add delete person functionality

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 
-const persons = [
+app.use(express.json());
+
+let persons = [
 	{
 		id: 1,
 		name: "Arto Hellas",
@@ -37,6 +39,13 @@ app.get("/api/persons/:id", (request, response) => {
 	} else {
 		response.status(404).end();
 	}
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+	const id = Number(request.params.id);
+	persons = persons.filter((person) => person.id !== id);
+
+	response.status(204).end();
 });
 
 app.get("/info", (request, response) => {
